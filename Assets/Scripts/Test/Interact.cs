7aeab066ae3cbc73ef2643601d44db4b6473b95e
Finiko <mybeddrom@gmail.com>
2017-06-22 20:45:02 +0700

    Physics

--- Assets/Scripts/Test/Interact.cs
+++ Assets/Scripts/Test/Interact.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Interact : MonoBehaviour {
-
+	public Text show;
 	// Use this for initialization
 	void Start () {
-	
+		generateText ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	void generateText() {
+
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+
+		}
 	}
 }
