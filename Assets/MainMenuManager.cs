d032d67a0dbbc2b2022916bb999bbafe17b70b29
Finiko <mybeddrom@gmail.com>
2017-07-04 18:04:59 +0700

    Main Menu with sounds

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+	public GameObject optionPanel;
+	public GameObject extraPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void startgame() {
+		int nextLevel;
+		nextLevel = Application.loadedLevel + 1;
+		Application.LoadLevel (nextLevel);
+	}
+	public void exit() {
+		if (Application.isEditor) {
+			UnityEditor.EditorApplication.isPlaying = false;
+		} else {
+			Application.Quit ();
+		}
+	}
+	public void option() {
+		optionPanel.SetActive (true);
+		gameObject.SetActive (false);
+	}
+	public void extra() {
+		extraPanel.SetActive (true);
+		gameObject.SetActive (false);
+	}
+}
--- Assets/Scripts/Test/playerController.cs
+++ Assets/Scripts/Test/playerController.cs
@@ -4,19 +4,22 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 	public float maxSpeed;
-	Rigidbody2D playerBody;
-	Animator playerAnimation;
-	bool facingRight;
-	bool touchGround = false;
-	float groundCheckRadius = 0.2f;
 	public LayerMask groundLayer;
+	public LayerMask ladderLayer;
 	public Transform groundChecker;
+	public BoxCollider2D ladderBoxChecker;
 	public float jumpForce;
 	public Text textcounter;
 	public Text interact;
-	private int soulCounter;
-	bool isOnLadder = false;
 	public float stairSpeed;
+	private int soulCounter;
+	private Rigidbody2D playerBody;
+	private Animator playerAnimation;
+	private bool facingRight;
+	private bool touchGround = false;
+	private float groundCheckRadius = 0.2f;
+	private bool isOnLadder = false;
+	private GameObject otherObject;
 	//public Transform launchPod;
 	//public GameObject missile;
 	//float fireRate = 0.5f;
@@ -29,44 +32,43 @@ public class playerController : MonoBehaviour {
 		facingRight = true;
 		soulCounter = 0;
 		setText ();
+		ladderBoxChecker = GetComponent<BoxCollider2D>();
+		otherObject = null;
 	}
 
 	void Update(){
-		if ((touchGround) && (Input.GetAxis ("Jump") > 0)) {
+
+		if ((touchGround) && isInputJump ()) {
 			touchGround = false;
-			playerAnimation.SetBool ("isTouchGround",touchGround); 
-			playerBody.AddForce(new Vector2(0,jumpForce));
+			jump ();
 		}
-		if (Input.GetAxisRaw("Fire1")>0) {
+		if  (isInputFire1()){
 			//firing();
-		}
-		float verticalSpeed;
-		if (isOnLadder == true) {
-			verticalSpeed = Input.GetAxis ("Vertical");
-			playerBody.velocity = new Vector2 (playerBody.velocity.x,verticalSpeed*stairSpeed);
-			isOnLadder = false;
 		}
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		touchGround = Physics2D.OverlapCircle (groundChecker.position, groundCheckRadius,groundLayer);
-		playerAnimation.SetBool ("isTouchGround",touchGround);
-		playerAnimation.SetFloat ("verticalSpeed", playerBody.velocity.y);
-
 		float horizontalSpeed;
-		horizontalSpeed = Input.GetAxis ("Horizontal");
-		playerBody.velocity = new Vector2 (horizontalSpeed*maxSpeed,playerBody.velocity.y);
-		playerAnimation.SetFloat("speed",Mathf.Abs (horizontalSpeed));
-		if ((horizontalSpeed>0)&&(!facingRight)){
-			flipFacing();
-		} else if ((horizontalSpeed<0)&&(facingRight)){
-			flipFacing();
+		float verticalSpeed;
+		horizontalSpeed = inputHorizontal ();
+		verticalSpeed = inputVertical ();
+
+		isOnGroundUpdate ();
+		jumpAnimation ();
+
+		moveHorizontal (horizontalSpeed);
+
+		if (isOnLadder == true) {
+			playerBody.gravityScale = 0;
+			playerBody.velocity = new Vector2 (playerBody.velocity.x,verticalSpeed*stairSpeed);
+			isOnLadderUpdate ();
 		}
 	}
 	void flipFacing() {
-		facingRight = !facingRight;
 		Vector3 newVector;
+
+		facingRight = !facingRight;
 		newVector = transform.localScale;
 		newVector.x = newVector.x * -1;
 		transform.localScale = newVector;
@@ -76,11 +78,15 @@ public class playerController : MonoBehaviour {
 			soulCounter = soulCounter + 1;
 			setText ();
 		} else if (other.gameObject.CompareTag ("Interact")) {
-			if (Input.GetAxisRaw ("Fire1") > 0) {
-				generateText ();
+			if (isInputFire1 ()) {
+				otherObject = other.gameObject;
 			}
 		} else if (other.gameObject.CompareTag ("Ladder")) {
 			isOnLadder = true;
+		} else if (other.gameObject.CompareTag ("Rope")) {
+
+		} else if (other.gameObject.CompareTag ("Enemy")) {
+			Destroy(gameObject);
 		}
 	}
 	void setText() {
@@ -88,6 +94,63 @@ public class playerController : MonoBehaviour {
 	}
 	void generateText() {
 		interact.text = soulCounter.ToString();
+	}
+	void isOnLadderUpdate() {
+		Vector2 pointA;
+		Vector2 pointB;
+		float playerX;
+		float playerY;
+		playerX = transform.position.x;
+		playerY = transform.position.y;
+		pointA = new Vector2 (playerX + ladderBoxChecker.offset.x - (ladderBoxChecker.transform.position.x/2), playerY + ladderBoxChecker.offset.y - (ladderBoxChecker.transform.position.y/2));
+		pointB = new Vector2 (playerX + ladderBoxChecker.offset.x + (ladderBoxChecker.transform.position.x/2), playerY + ladderBoxChecker.offset.y + (ladderBoxChecker.transform.position.y/2));
+		isOnLadder = Physics2D.OverlapArea (pointA,pointB,ladderLayer);
+		if (!isOnLadder) {
+			playerBody.gravityScale = 1;
+		}
+	}
+	void isOnGroundUpdate() {
+		touchGround = Physics2D.OverlapCircle (groundChecker.position, groundCheckRadius,groundLayer);
+	}
+	bool isInputJump() {
+		return (Input.GetAxis ("Jump") > 0);
+	}
+	bool isInputFire1() {
+		return (Input.GetAxisRaw("Fire1")>0);
+	}
+	float inputVertical() {
+		return(Input.GetAxis ("Vertical"));
+	}
+	float inputHorizontal() {
+		return (Input.GetAxis ("Horizontal"));
+	}
+	void jump() {
+		playerAnimation.SetBool ("isTouchGround",touchGround); 
+		playerBody.AddForce(new Vector2(0,jumpForce));
+	}
+	void climbLadder() {
+		Vector2 titikAwal;
+		Vector2 titikAkhir;
+		float x;
+		float y;
+		bool allowUp;
+		bool allowDown;
+		bool allowHorizontal;
+		x = transform.position.x;
+		y = transform.position.y;
+	}
+	void jumpAnimation() {
+		playerAnimation.SetBool ("isTouchGround",touchGround);
+		playerAnimation.SetFloat ("verticalSpeed", playerBody.velocity.y);
+	}
+	void moveHorizontal(float horizontalSpeed) {
+		playerBody.velocity = new Vector2 (horizontalSpeed*maxSpeed,playerBody.velocity.y);
+		playerAnimation.SetFloat("speed",Mathf.Abs (horizontalSpeed));
+		if ((horizontalSpeed>0)&&(!facingRight)){
+			flipFacing();
+		} else if ((horizontalSpeed<0)&&(facingRight)){
+			flipFacing();
+		}
 	}
 	/*void firing() {
 		if (Time.time > reloadTime) {
