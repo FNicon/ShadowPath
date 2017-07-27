3a115b328b24e4c7c119cd3156d3ef284c445ece
Lazuardi Firdaus <Lazuardi Firdaus>
2017-07-27 17:21:41 +0700

    death animation implemented

--- Assets/Scripts/Test/Death.cs
+++ Assets/Scripts/Test/Death.cs
@@ -7,25 +7,26 @@ public class Death : MonoBehaviour {
     public playerController player;
     public GameObject deathScreen;
     private bool deathCond = false;
+	private Animator animation;
 
-    public bool ping1 = false;
-    public bool ping2 = false;
-    public bool ping3 = false;
+    //public bool ping1 = false;
+    //public bool ping2 = false;
+    //public bool ping3 = false;
 
 
     // Use this for initialization
     void Start () {
-        ping1 = true;
+        //ping1 = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ping2 = true;
+        //ping2 = true;
 	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        ping3 = true;
+        //ping3 = true;
         if (other.gameObject.CompareTag("Enemy"))
         {
             die();
