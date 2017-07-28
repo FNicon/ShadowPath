cc973e6cca12b6e0bd8665a4e098b2aa6c16777c
Finiko <mybeddrom@gmail.com>
2017-07-28 13:30:05 +0700

    Change : Draft Village

---
+++ Assets/Scripts/Enemy/Enemy2.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour {
+	public bool movingRight;
+	public bool facingRight;
+	public float rightTarget;
+	public float leftTarget;
+	private float currentTarget;
+	public float maxSpeed;
+	private float currentSpeed;
+	private float negator;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		movingRight = true;
+		body = GetComponent<Rigidbody2D>();
+		negator = 1f;
+		currentTarget = rightTarget;
+		currentSpeed = maxSpeed;
+		facingRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate()
+	{
+		if (movingRight && transform.position.x > currentTarget)
+		{
+			negator = negator * -1;
+			currentTarget = leftTarget;
+			movingRight = false;
+			//FlipFacing();
+			currentSpeed = maxSpeed;
+		}
+		else if (!movingRight && transform.position.x < currentTarget)
+		{
+			negator = negator * -1;
+			currentTarget = rightTarget;
+			movingRight = true;
+			//FlipFacing();
+			currentSpeed = maxSpeed;
+		}
+		else
+		{
+
+		}
+		body.velocity = new Vector2(negator * currentSpeed, body.velocity.y);
+	}
+}
